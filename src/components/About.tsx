b3ec87d35f4c1088addf0600c665e17fc4b396f1
noabakergillis <noarosebkr@gmail.com>
2024-02-18 17:16:53 -0500

    more responsive design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,12 @@ const About: React.FC = () => {
   return (
     <div className="profile-block">
       <img className="main-image" src={profilePic} alt="profile-pic" />
-      <div style={{ width: "50%" }}>
+      <div className="profile-block-div">
         <div>
-          Hi! I'm a design engineer based in Philadelphia. I'm passionate
-          about exploring the intersection of design and engineering. I love
-          making things that are beautiful and/or functional. I'm looking for a
-          job doing just that, and I'm open to relocating for the right
-          adventure.
+          Hi! I'm a design engineer based in Philadelphia. I'm passionate about
+          exploring the intersection of design and engineering. I love making
+          things that are beautiful and/or functional. I'm looking for a job
+          doing just that, and I'm open to relocating for the right adventure.
         </div>
         <div style={{ marginTop: "25px" }}>
           Scroll down to see some recent work.
